27d43956767e6ae349a7ff118b8f02abb0f5db85
Parsa <pramezani92@gmail.com>
2025-10-06 20:45:11 +0330

    fix: fix refresh token and verify token

--- src/apps/authentication/tests/selectors/get_user.py
+++ src/apps/authentication/tests/selectors/get_user.py
@@ -2,9 +2,7 @@ import uuid
 
 from django.test import TestCase
 
-from src.apps.authentication.selectors import (
-    get_user_by_id,
-)
+from src.apps.authentication.selectors import get_user_by_id, get_user_by_phone_number
 from src.utils.fakers import UserFactory
 
 
@@ -17,3 +15,8 @@ class GetUserSelectorTestCase(TestCase):
         user = get_user_by_id(user_id=self.user.id)
         self.assertEqual(self.user, user)
         self.assertIsNone(get_user_by_id(self.fake_id))
+
+    def test_get_user_by_phone_number_selector(self):
+        user = get_user_by_phone_number(phone_number=self.user.phone_number)
+        self.assertEqual(self.user, user)
+        self.assertIsNone(get_user_by_phone_number(self.fake_id))
